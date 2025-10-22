cc743a19393f5f9676093a16d4fc543f9c0ae9a4
Vikas Anwla <89768990+VikasAnwla07@users.noreply.github.com>
2025-10-23 04:17:31 +0530

    Update AxiosInterceptor.jsx

--- src/Interceptor/AxiosInterceptor.jsx
+++ src/Interceptor/AxiosInterceptor.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
-  // baseURL: "https://jobhook-jhbj.onrender.com",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://job-portal-backend-khbn.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
